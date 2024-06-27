e9fe1fa130c8f7425b5100adab37139f7fbc00dd
Ahmed Mostafa <88963866+New-pro125@users.noreply.github.com>
2024-06-27 14:17:40 +0300

    Committing files

---
+++ Atcoder/C++ 20 (gcc 12.2)/abc326_a/46995868.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+const ll Mod= 1e9+7;
+const ld EPS=1e-6;
+const ll OO=INT_MAX;
+#define REP(i,a,b) for(int i=a;i<b;i++)
+#define REN(i,a,b) for(int i=a;i>b;i--)
+#define _STD_ std::ios::sync_with_stdio(false);std::cin.tie(nullptr);std::cout.tie(nullptr);
+#define _OPEN_ freopen("in.txt","r",stdin);freopen("out.txt","w",stdout);
+#define all(v)   (v).begin(),(v).end()
+template<typename t>
+ostream& operator <<(ostream&out,const vector<t>&vi){
+    for( const t&i:vi)
+        out<<i<<" ";
+    return out;
+}
+template<typename T>
+istream& operator >>(istream&out, vector<T>&vi){
+   for(auto&i:vi)
+    out>>i;
+    return out;
+}
+struct Edge{
+    int u,v,d;
+    Edge(int U,int V,int D):u(U),v(V),d(D){}
+    bool operator > (Edge other)const{
+            return this->d > other.d;
+    }
+};
+// string hashing(int n){
+//     string hashed;
+//     while(n){
+//         hashed=hashed +(char)('a'+n%26) ;
+//         n/=26;
+//     }
+//     return hashed;
+// }
+
+void work(){ 
+   int n,m;
+   cin>>n>>m;
+    if(n<m)
+        cout<<((m-n<=2)?"Yes":"No")<<"\n";
+    else
+         cout<<((n-m<=3)?"Yes":"No")<<"\n";
+}
+int main(){
+   _STD_
+    #ifndef ONLINE_JUDGE
+        _OPEN_
+    #endif
+    ll n=1;
+    //cin>>n;
+    // while(n--)
+      work();
+}
