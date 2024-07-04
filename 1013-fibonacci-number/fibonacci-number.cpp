872421a2eb34eb372f6a7d57ff2c2aaf71a70d84
Ahmed Mostafa <88963866+New-pro125@users.noreply.github.com>
2024-07-04 20:36:09 +0300

    Time: 0 ms (100.00%) | Memory: 7 MB (71.68%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,10 +1,18 @@
 class Solution {
-public:
-    int fib(int n) {
+vector<int>dp;
+private:
+int fibSol(int n) {
         if(n<0)
             return 0;
        if(n==0||n==1)
         return n;
-        return fib(n-1)+fib(n-2); 
+        if(dp[n]!=-1)
+            return dp[n];
+        return dp[n] = fibSol(n-1)+fibSol(n-2); 
+    }
+public:
+    int fib(int n) {
+       dp = vector<int>(31,-1);
+       return fibSol(n); 
     }
 };
