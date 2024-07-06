5b37af005bf037dc9c317dff5e6c3cfdacd82297
Ahmed Mostafa <88963866+New-pro125@users.noreply.github.com>
2024-07-06 12:14:26 +0300

    Time: 0 ms (100.00%) | Memory: 7.2 MB (30.58%) - LeetSync

--- 2645-pass-the-pillow/pass-the-pillow.cpp
+++ 2645-pass-the-pillow/pass-the-pillow.cpp
@@ -1,12 +1,15 @@
 class Solution {
+    int fast_mod(const int input, const int ceil) {
+    return input >= ceil ? input % ceil : input;
+}
 public:
     int passThePillow(int n, int time) {
         // 1 --> N  (N-1) && N -> 1 (N-1)
         int flag= time/(n-1);
-        if(flag%2){
-            return n-(time%(n-1));
+        if(fast_mod(flag,2)){
+            return n-fast_mod(time,n-1);
         }
         else
-            return time%(n-1)+1;
+            return fast_mod(time,n-1)+1;
     }
 };
