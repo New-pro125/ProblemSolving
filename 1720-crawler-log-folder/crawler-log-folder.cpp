dafac4b70fe1d2010cad4485e65cdeeed6ccb041
Ahmed Mostafa <88963866+New-pro125@users.noreply.github.com>
2024-07-10 14:44:44 +0300

    Time: 4 ms (67.13%) | Memory: 13.8 MB (96.60%) - LeetSync

---
+++ 1720-crawler-log-folder/crawler-log-folder.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        int steps = 0 ;
+        for(int i =0;i<logs.size();i++){
+            if(logs[i]=="../") steps=max(0,steps-1);
+            else if(logs[i]=="./") continue;
+            else
+                steps+=1;
+        }
+        return steps;
+    }
+};
