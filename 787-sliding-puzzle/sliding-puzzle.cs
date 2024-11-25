fc66b524c8eb3d7ced026e10c0df4ed33000f89f
Ahmed Mostafa <88963866+New-pro125@users.noreply.github.com>
2024-11-25 07:09:16 +0200

    Time: 16 ms (55.56%) | Memory: 44.3 MB (68.75%) - LeetSync

--- 787-sliding-puzzle/sliding-puzzle.cs
+++ 787-sliding-puzzle/sliding-puzzle.cs
@@ -9,15 +9,36 @@ public class Solution {
         new int[] {4,2}
     };
     public int SlidingPuzzle(int[][] board) {
+        string target = "123450";
         StringBuilder startState = new StringBuilder();
         for(int i =0 ;i<2;i++){
             for(int j = 0;j<3;j++){
                 startState.Append(board[i][j]);
             }
         }
-        Dictionary<string,int> visited = new Dictionary<string,int>();
-        Dfs(startState.ToString(),visited,startState.ToString().IndexOf('0'),0);
-        return visited.ContainsKey("123450") ? visited["123450"] : -1 ; 
+        HashSet<string>visited = new HashSet<string>();
+        Queue<string>queue = new Queue<string>();
+        queue.Enqueue(startState.ToString());
+        visited.Add(startState.ToString());
+        int moves = 0;
+        while(queue.Count > 0){
+            int size = queue.Count;
+            for(int i = 0;i<size;i++){
+                string currentState = queue.Dequeue();
+                if(currentState == target){
+                    return moves;
+                }
+                int zeroPos = currentState.IndexOf('0');
+                foreach(int newPos in directions[zeroPos]){
+                    string nextState  = Swap(currentState,zeroPos,newPos);
+                    if(visited.Contains(nextState)) continue;
+                    visited.Add(nextState);
+                    queue.Enqueue(nextState);
+                }
+            }
+            moves++;
+        }
+        return  -1 ; 
     }
     private void Dfs(string state,Dictionary<string,int>visited,int zeroPos,int moves){
         if(visited.ContainsKey(state) && visited[state]<=moves){
