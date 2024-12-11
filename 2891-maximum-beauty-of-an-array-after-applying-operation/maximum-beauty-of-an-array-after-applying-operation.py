37beef019fca46b9fcb381d2f2a9f473e363661f
Ahmed Mostafa <88963866+New-pro125@users.noreply.github.com>
2024-12-11 12:16:57 +0200

    Time: 155 ms (89.32%) | Memory: 28.1 MB (39.52%) - LeetSync

--- 2891-maximum-beauty-of-an-array-after-applying-operation/maximum-beauty-of-an-array-after-applying-operation.py
+++ 2891-maximum-beauty-of-an-array-after-applying-operation/maximum-beauty-of-an-array-after-applying-operation.py
@@ -1,14 +1,3 @@
 class Solution:
     def maximumBeauty(self, nums: List[int], k: int) -> int:
-        # sweep line algorithm : 
-        numbers = []
-        for num in nums:
-            numbers.append((num-k,1))
-            numbers.append((num+k+1,-1))
-        numbers.sort()
-        max_beauty = 0
-        current_count =0
-        for value,effect in numbers:
-            current_count +=effect
-            max_beauty = max(max_beauty,current_count)
-        return max_beauty
+        return len(nums:=sorted(nums))-reduce(lambda i,v:i+(v-nums[i]>2*k),nums,0)
