63d085c4d549658fb5d6b9a4534bdbd4c22d7ab0
Ahmed Mostafa <88963866+New-pro125@users.noreply.github.com>
2025-03-06 22:56:10 +0200

    Time: 1178 ms (66.35%) | Memory: 47.4 MB (51.22%) - LeetSync

--- 2375-minimum-obstacle-removal-to-reach-corner/minimum-obstacle-removal-to-reach-corner.py
+++ 2375-minimum-obstacle-removal-to-reach-corner/minimum-obstacle-removal-to-reach-corner.py
@@ -3,18 +3,20 @@ class Solution:
         self, grid: List[List[int]] = [[0, 1, 1], [1, 1, 0], [1, 1, 0]]
     ) -> int:
         rows, cols = len(grid), len(grid[0])
-        min_heap = [(0, 0, 0)]  # (obstacles,r,c)
+        q = deque([(0, 0, 0)])  # (obstacles,r,c)
         visited = set([(0, 0)])  #
-        while min_heap:
-            obstacles, r, c = heapq.heappop(min_heap)
+        while q:
+            obstacles, r, c = q.popleft()
             nei = [[r + 1, c], [r - 1, c], [r, c + 1], [r, c - 1]]
             if (r, c) == (rows - 1, cols - 1):
                 return obstacles
             for nr, nc in nei:
                 if nr >= rows or nr < 0 or nc >= cols or nc < 0 or (nr, nc) in visited:
                     continue
-
-                heapq.heappush(min_heap, (obstacles + grid[nr][nc], nr, nc))
+                if grid[nr][nc]:
+                    q.append((obstacles + grid[nr][nc], nr, nc))
+                else:
+                    q.appendleft((obstacles + grid[nr][nc], nr, nc))
                 visited.add((nr, nc))
 
         return -1
