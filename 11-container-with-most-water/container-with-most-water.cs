a3094c93efe2902c4e552a4e363b6cf40509e332
Ahmed Mostafa <88963866+New-pro125@users.noreply.github.com>
2025-07-14 12:56:53 +0300

    Time: 1 ms (100.00%) | Memory: 63.2 MB (74.29%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cs
+++ 11-container-with-most-water/container-with-most-water.cs
@@ -2,10 +2,14 @@ public class Solution {
     public int MaxArea(int[] height) {
         int left = 0, right = height.Length - 1, area = 0, high = 0;
         while(left < right){
-            high = Math.Min(height[left],height[right]);
-            area = Math.Max(area,high * (right - left));
-            while(left < right && height[left] <=high) left++;
-            while(left < right && height[right] <=high) right--;
+            area = Math.Max(area,(right-left) * Math.Min(height[left],height[right]));
+            if(height[left] <= height[right]) {
+                left++;
+            }
+            else {
+                right--;
+            }
+
         }
         return area;
     }
