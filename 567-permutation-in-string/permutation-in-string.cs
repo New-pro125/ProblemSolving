63c99a3e83baf1552aac47a9d88ccdc8e5ba737b
Ahmed Mostafa <88963866+New-pro125@users.noreply.github.com>
2025-07-14 16:04:21 +0300

    Time: 25 ms (21.81%) | Memory: 43.6 MB (53.89%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public bool CheckInclusion(string s1, string s2) {
+        if(s1.Length > s2.Length) return false;
+        int[] s1Count = new int[26];
+        int[] s2Count = new int[26];
+        for(int i= 0;i<s1.Length;i++){
+            s1Count[s1[i] - 'a']++;
+        }
+        for(int i =0;i<s2.Length;i++){
+            s2Count[s2[i] - 'a']++;
+            if(i >= s1.Length){
+                s2Count[s2[i - s1.Length] -'a']--;
+            }
+            if(s1Count.SequenceEqual(s2Count)) return true;
+        }
+        return false;
+    }
+}
