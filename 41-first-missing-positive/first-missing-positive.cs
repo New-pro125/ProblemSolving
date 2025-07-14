925fe3492391d9b8a0f25d1442fb9bc28fd52e89
Ahmed Mostafa <88963866+New-pro125@users.noreply.github.com>
2025-07-14 09:52:06 +0300

    Time: 2 ms (73.43%) | Memory: 63.9 MB (89.57%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cs
+++ 41-first-missing-positive/first-missing-positive.cs
@@ -1,28 +1,26 @@
 public class Solution {
     public int FirstMissingPositive(int[] nums) {
          int n = nums.Length;
-         bool contains1 = false;
-         for(int i =0;i<n;i++){
-            if(nums[i] ==1){
-                contains1 = true;
-            }
-            else if(nums[i] <=0 || nums[i] > n)
-                nums[i] =1;
-         }
-         if(!contains1) return 1;
-         for(var i = 0;i<nums.Length;i++){
-            var value = Math.Abs(nums[i]);
-            if(value == n){
-                nums[0] = -Math.Abs(nums[0]);
-            }
-            else {
-                nums[value] = - Math.Abs(nums[value]);
-            }
-         }
-         for(var i = 1;i<n;i++){
-            if(nums[i] > 0) return i;
-         }
-         if(nums[0] > 0) return n;
-         return n+1;
+           int i = 0;
+  while(i < n)
+  {
+      var correctIdx = nums[i] - 1;
+      if (nums[i] > 0 && nums[i]<=n && nums[i] != nums[correctIdx])
+      {
+          (nums[i], nums[correctIdx]) = (nums[correctIdx], nums[i]);
+      }
+      else
+      {
+          i++;
+      }
+  }
+  for(i = 0; i < n; i++)
+  {
+      if (nums[i] != i + 1)
+      {
+          return i + 1;
+      }
+  }
+  return n + 1;
     }
 }
