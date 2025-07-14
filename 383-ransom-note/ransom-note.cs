73f9f73b3f6dd18c626f7373c19f9035bfd55a98
Ahmed Mostafa <88963866+New-pro125@users.noreply.github.com>
2025-07-14 11:30:41 +0300

    Time: 0 ms (100.00%) | Memory: 45.6 MB (98.49%) - LeetSync

--- 383-ransom-note/ransom-note.cs
+++ 383-ransom-note/ransom-note.cs
@@ -1,15 +1,15 @@
 public class Solution {
     public bool CanConstruct(string ransomNote, string magazine) {
-         int[] arr = new int[26];
- foreach (char c in magazine)
- {
-     arr[c - 'a']++;
- }
- foreach (char c in ransomNote)
- {
-     if (arr[c - 'a'] <= 0) return false;
-     arr[c - 'a']--;
- }
- return true;
-    }
+        Span<int> letters = stackalloc int[26];
+foreach (char c in magazine)
+{
+    letters[c - 'a']++;
+}
+foreach (char c in ransomNote)
+{
+    if (letters[c - 'a'] <= 0) return false;
+    letters[c - 'a']--;
+}
+return true;
+}
 }
