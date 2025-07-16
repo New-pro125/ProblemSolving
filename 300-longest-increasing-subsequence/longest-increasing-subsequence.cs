5b6368a42b3ed1e3a12be26b985983dbf9b2f912
Ahmed Mostafa <88963866+New-pro125@users.noreply.github.com>
2025-07-16 11:48:57 +0300

    Time: 403 ms (9.34%) | Memory: 82.3 MB (8.46%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cs
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cs
@@ -1,27 +1,17 @@
 public class Solution {
-    private int LengthOfLISHelper(IReadOnlyList<int> nums,int idx,int prevIdx,int?[,] memo)
-    {
-        if(idx == nums.Count)
-        {   
-            return 0;
-        }
-        if (memo[prevIdx + 1 , idx] != null)
-        {
-            return memo[prevIdx + 1 , idx].Value;
-        }
-        int ans = 0;
-
-    // take  this number
-        if(prevIdx == -1 || nums[prevIdx] < nums[idx])
-            ans = Math.Max(ans , (1 + LengthOfLISHelper(nums, idx + 1, idx,memo)));
-    // leave this number
-        ans = Math.Max(ans, (LengthOfLISHelper(nums,idx + 1, prevIdx, memo)));
-        memo[prevIdx + 1, idx] = ans;
-        return ans;
-    }
     public int LengthOfLIS(int[] nums) {
         int n = nums.Length;
-        int?[,] memo = new int?[n + 1, n];
-        return LengthOfLISHelper(nums,0,-1,memo);
+        int[,] memo = new int[n + 1, n+1];
+        for(int idx = n - 1; idx >= 0; idx--)
+            {
+                for(int prevIdx = idx-1;prevIdx >= -1; prevIdx--)
+                    {
+                        if (prevIdx == -1 || nums[idx] > nums[prevIdx])
+                            memo[prevIdx + 1, idx] = Math.Max(memo[prevIdx + 1, idx], 1 + memo[idx + 1, idx + 1]);
+                        memo[prevIdx + 1, idx] = Math.Max(memo[prevIdx + 1, idx], memo[prevIdx + 1, idx + 1]);
+                    }
+            }
+              
+        return memo[0,0];
     }
 }
