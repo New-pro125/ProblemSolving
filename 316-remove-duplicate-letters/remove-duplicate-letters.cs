d7e9c260b9593912e542de7d23af3586f0cfa806
Ahmed Mostafa <88963866+New-pro125@users.noreply.github.com>
2025-07-16 09:58:47 +0300

    Time: 10 ms (41.46%) | Memory: 42.5 MB (9.76%) - LeetSync

--- 316-remove-duplicate-letters/remove-duplicate-letters.cs
+++ 316-remove-duplicate-letters/remove-duplicate-letters.cs
@@ -1,31 +1,27 @@
 public class Solution {
     public string RemoveDuplicateLetters(string s) {
-    int n = s.Length;
-    int[] lastIndex = new int[26];
-    bool[] used = new bool[26];
-    Stack<char> stack = new Stack<char>();
+         int n = s.Length;
+ Span<int> freq = stackalloc int[26];
+ BitArray inStack = new BitArray(26);
+ Stack<char> temp = new();
+ for (int i = 0; i < n; i++)
+ {
+     freq[s[i] - 'a'] = i;
 
-     for (int i = 0; i < n; i++){
-
-         lastIndex[s[i] - 'a'] = i; 
+ }
+ for(int i = 0; i < n; i++)
+ {
+     if(inStack[s[i]-'a']) continue;
+     
+     while (temp.Count > 0 && temp.Peek() > s[i] && freq[temp.Peek() - 'a'] > i)
+     { 
+         inStack[temp.Pop() - 'a'] = false;
      }
-
-    for(int i = 0;i < n;i++){
-       char c = s[i];
-
-      if(used[c-'a'])  continue;
-        
-      while(stack.Count > 0 && stack.Peek() > c && lastIndex[stack.Peek() - 'a'] > i){
-
-        char removed = stack.Pop();
-        used[removed - 'a'] = false;
-      }
-      stack.Push(c);
-      used[c - 'a'] = true;
-   }
-
-  char[] result = stack.Reverse().ToArray(); 
-  return new string(result);
-
+     
+         temp.Push(s[i]);
+         inStack[s[i] - 'a'] = true;
+     
+ }
+ return new string(temp.Reverse().ToArray());
     }
 }
