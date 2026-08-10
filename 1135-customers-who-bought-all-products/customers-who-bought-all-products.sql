a7a9308d34f836f78f1cc10fadde6ad7756557bd
Ahmed Mostafa <88963866+New-pro125@users.noreply.github.com>
2026-08-10 12:18:31 +0300

    Time: 490 ms (74.43%) | Memory: 0B (100.00%) - LeetSync

--- 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
+++ 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
@@ -1,11 +1,7 @@
 -- Write your PostgreSQL query statement below
-WITH CTE AS
-(SELECT customer_id, COUNT(DISTINCT product_key)
-FROM Customer
-GROUP BY customer_id
-HAVING COUNT(DISTINCT product_key) IN (
-    SELECT COUNT(DISTINCT product_key) 
-    FROM Product
-))
 SELECT customer_id 
-FROM CTE;
+FROM customer
+GROUP BY customer_id
+HAVING COUNT(DISTINCT product_key ) =
+(SELECT COUNT(*) 
+FROM product)
