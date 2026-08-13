4a2a899f5d14117f1173c3eff389a8dfc5adce73
Ahmed Mostafa <88963866+New-pro125@users.noreply.github.com>
2026-08-13 09:37:35 +0300

    Time: 221 ms (12.57%) | Memory: 0B (100.00%) - LeetSync

---
+++ 3530-odd-and-even-transactions/odd-and-even-transactions.sql
@@ -0,0 +1,5 @@
+-- Write your PostgreSQL query statement below
+SELECT transaction_date, SUM(CASE WHEN amount %2 = 1 then amount else 0 end) as odd_sum, SUM(CASE WHEN amount %2 = 0 then amount else 0 end) as even_sum
+FROM transactions
+GROUP BY transaction_date
+order by transaction_date asc;
